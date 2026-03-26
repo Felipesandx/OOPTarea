4956565ab5234761e381692b3ca67cddcfd4fa1a
Felipesandx <jsandovalr9@miumg.edu.gt>
2026-03-26 15:40:44 -0600

    problema 2 terminado

--- src/com/encapsulacion/Main.java
+++ src/com/encapsulacion/Main.java
@@ -3,8 +3,13 @@ package com.encapsulacion;
 public class Main {
 	public static void main(String[] args) {
 		//clasePersona();
-	
+		Rectangulo r1 = new Rectangulo(10,25);
+		Rectangulo r2 = new Rectangulo (50, 5);
+		
+		r1.compararCon(r2);
+		
 	}
+		
 	public static void clasePersona() {
 		Persona p1 = new Persona("Juan", "Sandoval", 20);
 		Persona p2 = new Persona("Alejandro", "Hernandez", 32);
--- src/com/encapsulacion/Rectangulo.java
+++ src/com/encapsulacion/Rectangulo.java
@@ -9,6 +9,30 @@ public class Rectangulo {
 		this.altura = altura;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return "Rectangulo [base=" + base + ", altura=" + altura + "]";
+	}
 
+	public int area() {
+		return base*altura;
+	}
+	public int perimetro() {
+		
+		return 2 *(base+altura);
+	}
+	
+	public void compararCon(Rectangulo otro) {
+		int areaRec1 = this.area();
+		int areaRec2 = otro.area();
+		
+		if (areaRec1 > areaRec2) {
+			System.out.println("El área mayor es de rectangulo 1 =  " + areaRec1);
+		}else if (areaRec1 == areaRec2) {
+			System.out.println("Ambas áreas son iguales ");
+		}else {
+			System.out.println("El área mayor es de rectangulo 2 =  " + areaRec2);
+		}
+	}
+		
 }
